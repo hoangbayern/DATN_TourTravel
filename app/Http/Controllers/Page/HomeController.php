14eb49603c1ca3cf11fb0828476eb0716c2443a4
SypHoang <letiendat@syp.vn>
2024-04-19 16:18:42 +0700

    feature authenticate client

---
+++ app/Http/Controllers/Page/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Page;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Location;
+use App\Models\Tour;
+use App\Models\Article;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('page.home.index');
+    }
+
+    public function contact()
+    {
+        return view('page.contact.index');
+    }
+
+    public function about()
+    {
+        return view('page.about.index');
+    }
+
+    public function transport()
+    {
+        return view('page.transport.index');
+    }
+
+    public function changeReturn()
+    {
+        return view('page.return.index');
+    }
+
+    public function security()
+    {
+        return view('page.security.index');
+    }
+}
